ac2757f52dc9e9f8d2fecbe5287420809da519ab
LAIS TEIXEIRA DE FREITAS <lais.freitas7>
2024-06-18 08:54:29 -0300

    18/06

--- client/src/app/ApiEdit/page.tsx
+++ client/src/app/ApiEdit/page.tsx
@@ -1,14 +1,22 @@
 "use client"
 import styles from './page.module.css'
+import Image from 'next/image'
 
 export default function apiEdit(){
     return(
         <main className={styles.main}>
             <div className={styles.coverPhoto}>
-                <input type="image" name="" id="" />
+                <input className={styles.addCover} type="image" />
             </div>
             <div className={styles.profilePhoto}>
-                
+                <Image 
+                src="ApiProfile.svg"
+                alt='Beekeeper icon'
+                width={206}
+                height={206}
+                priority
+                />
+
             </div>
 
         </main>
