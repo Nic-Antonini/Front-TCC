da0fce5841b8ef47840aeea2812f1b0b51585ebd
LAIS TEIXEIRA DE FREITAS <lais.freitas7>
2024-06-18 07:58:21 -0300

    18/006

--- client/src/app/ApiEdit/page.tsx
+++ client/src/app/ApiEdit/page.tsx
@@ -1,7 +1,16 @@
+"use client"
+import styles from './page.module.css'
+
 export default function apiEdit(){
     return(
-        <>
-            
-        </>
+        <main className={styles.main}>
+            <div className={styles.coverPhoto}>
+                <input type="image" name="" id="" />
+            </div>
+            <div className={styles.profilePhoto}>
+                
+            </div>
+
+        </main>
     )
 }
