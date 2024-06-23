40ec01c16fa6b8c843bb93aa6818e5a6fdec9e43
Nic-Antonini <121265107+Nic-Antonini@users.noreply.github.com>
2024-06-23 03:36:52 -0300

    23/06

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,26 +1,37 @@
 "use client";
-import React, {useState, useLayoutEffect} from 'react';
+import React, {useState, useEffect,useLayoutEffect} from 'react';
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap-trial/ScrollTrigger';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
-
 
 export default function Home() {
   const [imagemSrc, setImagemSrc] = useState('/toggleNavbar.svg');
   const [linksVisiveis, setLinksVisiveis] = useState(false); 
 
-  useLayoutEffect(()=>{
-    gsap.to(`.${styles.descImg}`, {
-      duration: 1, x: 0, opacity: 1, 
-    })
+  useLayoutEffect(() => {
+    const descImgElement = document.querySelector(`.${styles.descImg}`);
+    if (descImgElement) {
+      gsap.to(descImgElement, {
+        duration: 1, x: 0, opacity: 1, 
+      });
+      return () =>{
+        gsap.killTweensOf(`.${styles.descImg}`)
+    }
+    }
   }, []);
 
-  useLayoutEffect(()=>{
-    gsap.to(`.${styles.textArea}`, {
-      duration: 2, opacity: 1
-    })
+  useLayoutEffect(() => {
+    const textAreaElement = document.querySelector(`.${styles.textArea}`);
+    if (textAreaElement) {
+      gsap.to(textAreaElement, {
+        duration: 2, opacity: 1
+      });
+      return () =>{
+        gsap.killTweensOf(`.${styles.textArea}`)
+    }
+    }
   }, []);
 
   const handleClick = () => {
@@ -32,11 +43,45 @@ export default function Home() {
       setLinksVisiveis(false); 
     }
   };
+  
+  useLayoutEffect(() =>{
+  gsap.registerPlugin(ScrollTrigger)
+  gsap.to(`.${styles.apiary}`, {
+    right: 0, 
+    duration: 1,
+    scrollTrigger: {
+      trigger: `.${styles.containerAboutUs}`,
+      start: "top 400px",
+      end: "bottom 500px",
+      scrub: true
+    }
+  })
+  return () =>{
+      gsap.killTweensOf(`.${styles.apiary}`)
+  }
+  }, [])
+
+  useLayoutEffect(() =>{
+    gsap.registerPlugin(ScrollTrigger)
+    gsap.to([`.${styles.aboutUs}`, `.${styles.titleAboutUs}`], {
+      opacity: 1,
+      duration: 2,
+      scrollTrigger: {
+        trigger: `.${styles.containerAboutUs}`,
+        start: "top 400px",
+        end: "bottom 500px",
+        scrub: true
+      }
+    })
+    return () =>{
+        gsap.killTweensOf([`.${styles.apiary}`, `.${styles.titleAboutUs}`])
+    }
+    }, [])
+
   return (
     <main className={styles.main}>
-      
-        <header className={styles.header}>
-          <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.container}>
           <Image src="/logo.svg"
             alt="Logo do sistema BeeTech"
             width={190}
@@ -46,38 +91,38 @@ export default function Home() {
           />
 
           <nav className={styles.nav}>
-            <a href="/" className={styles.navLink}>Como Funciona</a>
+            <a href="/#ComoFunciona" className={styles.navLink}>Como Funciona</a>
             <a href="/" className={styles.navLink}>Contato</a>
             <Link href={"/"} className={styles.navLink}>FAQ</Link>
-            <a href="/" className={styles.navLink}>Sobre Nós</a>
+            <a href="/#SobreNós" className={styles.navLink}>Sobre Nós</a>
             <Link href={"/"} className={styles.navLink}>Entrar</Link>
             <Link href={"/"}><button className={styles.navBtn}>Cadastrar</button></Link>
           </nav>
 
           <div className={styles.toggleNavbar}>
-      <button className={styles.menu} onClick={handleClick}>
-        <Image 
-          src={imagemSrc}
-          alt="Botão para abrir o menu"
-          width={48}
-          height={48}
-          priority
-        />
-      </button>
-      {linksVisiveis && (
-        <>
-          <a href="/" className={styles.navLink1}>Como Funciona</a>
-          <a href="/" className={styles.navLink1}>Contato</a>
-          <Link href={"/"} className={styles.navLink1}>FAQ</Link>
-          <a href="/" className={styles.navLink1}>Sobre Nós</a>
-          <Link href={"/"} className={styles.navLink1}>Entrar</Link>
-          <Link href={"/"}><button className={styles.navBtn1}>Cadastrar</button></Link>
-        </>
-      )}
-    </div>
-
+            <button className={styles.menu} onClick={handleClick}>
+              <Image 
+                src={imagemSrc}
+                alt="Botão para abrir o menu"
+                width={48}
+                height={48}
+                priority
+              />
+            </button>
+            {linksVisiveis && (
+              <>
+                <a href="/" className={styles.navLink1}>Como Funciona</a>
+                <a href="/" className={styles.navLink1}>Contato</a>
+                <Link href={"/"} className={styles.navLink1}>FAQ</Link>
+                <a href="/" className={styles.navLink1}>Sobre Nós</a>
+                <Link href={"/"} className={styles.navLink1}>Entrar</Link>
+                <Link href={"/"}><button className={styles.navBtn1}>Cadastrar</button></Link>
+              </>
+            )}
           </div>
-        <section className={styles.desc}>
+        </div>
+
+        <div className={styles.desc}>
           <div className={styles.textArea}>
             <h1 className={styles.textDesc}>
               Unindo <span className={styles.span}> Criadores de Abelhas </span>
@@ -87,17 +132,41 @@ export default function Home() {
             </h1>
           </div>
 
-            <Image src="/descImg.svg"
+          <Image src="/descImg.svg"
             alt="Ilustração de um apicultor e um agricultor unidos"
             width={310}
             height={405}
             priority
             className={styles.descImg}/>
-        </section>
+        </div>
       </header>
+      <section id='SobreNós' className={styles.containerAboutUs}>
+        <Image src={"/apiary.svg"}
+        width={370}
+        height={480}
+        alt='Desenho de uma colméia'
+        className={styles.apiary}
+        />
+        <div className={styles.textAboutUs}>
+            <h1 className={styles.titleAboutUs}>
+            Sobre nós
+            </h1>
+            <p className={styles.aboutUs}>
+            A BeeTech é uma plataforma inovadora que conecta apicultores e agricultores,
+            facilitando a negociação de serviços de polinização. Nosso objetivo é criar
+            uma ponte direta entre esses dois públicos, permitindo que encontrem o parceiro
+            ideal para suas necessidades específicas. A polinização desempenha um papel
+            fundamental na agricultura, e nossa plataforma visa tornar esse processo mais
+            acessível e eficiente para todos os envolvidos.
+            </p>
+        </div>
+      </section>
+      <section id='ComoFunciona' className={styles.containerHow}>
+            h
+      </section>
+      <section id='Contato'>
 
-
-
+      </section>
     </main>
   );
 }
