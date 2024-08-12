b561126c6600f446331f976ed6c867b3ce5b587c
Nicolas Antonini <nicolasantonini06@gmail.com>
2024-08-12 16:55:28 -0300

    implement the sidebar homepage

--- client/src/app/Homepage/Components/page.tsx
+++ client/src/app/Homepage/Components/page.tsx
@@ -1,11 +1,54 @@
 import React from "react";
-import style from "./page.module.css"
-import {House} from "lucide-react"
+import style from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image"
+import {House, Search, MessageSquare, Settings} from "lucide-react";
 
 export default function Sidebar(){
     return(
         <aside className={style.sidebar}>
-            <House />
+            <nav className={style.navBar}>
+                <Link href={"/Homepage"} className={style.navLink}> <House/> Início </Link>
+                <Link href={"/AreaSearch"} className={style.navLink}> <Search/> Pesquisar </Link>
+                <Link href={"/Chat"} className={style.navLink}> <MessageSquare/> Chat </Link>
+                <Link href={"/Settings"} className={style.navLink}> <Settings/> Configurações </Link>
+            </nav>
+
+            <hr className={style.division} />
+
+            <nav className={style.secondNavBar}>
+                <p className={style.titleSecNavBar}>Últimas conversas</p>
+
+                <Link href={"/Chat/?"} className={style.navChat}>
+                    <Image priority
+                        src='/apiProfile.svg'
+                        width={40}
+                        height={40}
+                        alt="Foto de perfil de algum usuário"
+                        className={style.profileUserChat}/>
+                    Nome do usuário
+                </Link>
+
+                <Link href={"/Chat/?"} className={style.navChat}>
+                    <Image priority
+                        src='/apiProfile.svg'
+                        width={40}
+                        height={40}
+                        alt="Foto de perfil de algum usuário"
+                        className={style.profileUserChat}/>
+                    Nome do usuário
+                </Link>
+                <Link href={"/Chat/?"} className={style.navChat}>
+                    <Image priority
+                        src='/apiProfile.svg'
+                        width={40}
+                        height={40}
+                        alt="Foto de perfil de algum usuário"
+                        className={style.profileUserChat}/>
+                    Nome do usuário
+                </Link>
+
+            </nav>
         </aside>
     )
 }
