34cd512fbcd1a34dd8155d179327dd7e6d4b91e4
nicolas antonini <121265107+Nic-Antonini@users.noreply.github.com>
2024-08-12 10:55:33 -0300

    .

---
+++ client/src/app/Homepage/page.tsx
@@ -0,0 +1,16 @@
+import styles from './page.module.css'
+
+export default function user(){
+    return(
+        <div className={styles.body}>
+            <div>
+                <aside className={styles.sidebar}>
+
+                </aside>
+                <main className={styles.main}>
+
+                </main>
+            </div>
+        </div>
+    )
+}
