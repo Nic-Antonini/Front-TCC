e5fea33b2ebd9abf69ff5a22cc2ebf073f424949
Nic-Antonini <nicki.pablo123@gmail.com>
2024-08-20 03:19:45 -0300

    Add the main in Homepage

---
+++ client/src/app/Homepage/Components/carouselProfiles/EmblaCarousel.tsx
@@ -0,0 +1,45 @@
+'use client'
+import React, { useCallback } from 'react'
+import { EmblaOptionsType, EmblaCarouselType } from 'embla-carousel'
+import {
+  PrevButton,
+  NextButton,
+  usePrevNextButtons
+} from './EmblaCarouselArrowButtons'
+import useEmblaCarousel from 'embla-carousel-react'
+import styles from './embla.module.css'
+
+type PropType = {
+  slides: number[]
+  options?: EmblaOptionsType
+}
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props
+  const [emblaRef, emblaApi] = useEmblaCarousel(options)
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi)
+
+  return (
+    <section className={styles.embla}>
+    <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+      <div className={styles.embla__viewport} ref={emblaRef}>
+        <div className={styles.embla__container}>
+          {slides.map((index) => (
+            <div className={styles.embla__slide} key={index}>
+              <div className={styles.embla__slide__number}>{index + 1}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+    </section>
+  )
+}
+
+export default EmblaCarousel
--- client/src/app/Homepage/page.tsx
+++ client/src/app/Homepage/page.tsx
@@ -1,16 +1,69 @@
+'use client'
 import React from "react";
 import style from "./page.module.css";
-import Sidebar from './Components/page'
+import Sidebar from "./Components/sidebar/page";
+import Image from "next/image";
+import Link from "next/link";
+import EmblaCarousel from "./Components/carouselProfiles/EmblaCarousel";
+import { EmblaOptionsType } from "embla-carousel";
 
 export default function Homepage() {
-    return(
-        <div className={style.allPage}>
-            <div className={style.secondDiv}>
-                <Sidebar/>
-                <main className={style.main}>
-                
-                </main>
-            </div>
-        </div>
-    )
+
+    const OPTIONS: EmblaOptionsType = {
+        align: 'start',
+        dragFree: true,
+        loop: true,
+        slidesToScroll: 'auto',
+        duration: 60
+      }
+      const SLIDE_COUNT = 20
+      const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
+  return (
+    <div className={style.allPage}>
+      <div className={style.secondDiv}>
+        <Sidebar />
+        <main className={style.main}>
+          <div className={style.myProfileDiv}>
+            <Link href={"/MeuPerfil"}>
+              <Image
+                alt="Meu Perfil"
+                src="/AgriProfile.svg"
+                className={style.myProfile}
+                width={50}
+                height={50}
+                priority
+              />
+            </Link>
+          </div>
+          <div className={style.sections}>
+            <section className={style.sec1}>
+              <h1 className={style.secTitle}>
+                Apicultores que talvez você se interesse:
+              </h1>
+              <div className={style.profiles}>
+                <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+              </div>
+            </section>
+            <section className={style.sec}>
+              <h1 className={style.secTitle}>
+                Apicultores mais próximos de você:
+              </h1>
+              <div className={style.profiles}>
+                <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+              </div>
+            </section>
+            <section className={style.sec}>
+              <h1 className={style.secTitle}>
+                Apicultores em destaque:
+              </h1>
+              <div className={style.profiles}>
+                <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+              </div>
+            </section>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
