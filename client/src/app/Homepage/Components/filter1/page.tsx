24cdda45ca97962385d926990cdeb19a38f619ea
Nic-Antonini <nicki.pablo123@gmail.com>
2024-08-27 00:10:23 -0300

    .

--- client/src/app/Homepage/Components/filter1/page.tsx
+++ client/src/app/Homepage/Components/filter1/page.tsx
@@ -1,17 +1,40 @@
 import Link from "next/link";
 import { Search } from "lucide-react";
-
+import styles from './page.module.css'
 
 export default function filter1({}){
     return(
         <>
-            <h1 className="title">
-            Filtros de pesquisa
+            <h1 className={styles.title}>
+                Filtros de pesquisa
             </h1>
-            <p>Espécies de abelha</p>
-            <input type="checkbox" />
-            <p>
 
+            <p className={styles.beeSpecies}>Espécies de abelha</p>
+
+            <div className={styles.specie}>
+                <input type="checkbox" />
+                <p>
+                    espécie 1
+                </p>
+            </div>
+            <div className={styles.specie}>
+                <input type="checkbox" />
+                <p>
+                    espécie 2
+                </p>
+            </div>
+            <div className={styles.specie}>
+                <input type="checkbox" />
+                <p>
+                    espécie 3
+                </p>
+            </div>
+
+            <p>Disponibilidade de caixas</p>
+            <input type="text" className={styles.input} />
+
+            <p>
+                
             </p>
         </>
     )
