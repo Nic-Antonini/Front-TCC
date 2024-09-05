1507a499912974288ab7e92c1b0447d8c4ee2147
nicolas antonini <121265107+Nic-Antonini@users.noreply.github.com>
2024-09-05 14:44:35 -0300

    .

--- client/src/app/farmer/editProfile/page.tsx
+++ client/src/app/farmer/editProfile/page.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 type ImageType = 'profile' | 'cover';
 
 export default function EditProfile() {
-    const [profileImage, setProfileImage] = useState<string>('/default-profile.png'); // Imagem padrão do perfil
+    const [profileImage, setProfileImage] = useState<string>('/agriProfile.svg'); // Imagem padrão do perfil
     const [coverImage, setCoverImage] = useState<string>('/default-cover.png'); // Imagem padrão do cover
   
     const handleDrop = (acceptedFiles: File[], type: ImageType) => {
@@ -30,13 +30,22 @@ export default function EditProfile() {
                 <Dropzone onDrop={(files: File[], _: FileRejection[], __: DropEvent) => handleDrop(files, 'cover')}>
                     {({ getRootProps, getInputProps }) => (
                         <div {...getRootProps()} className={styles.imageContainer}>
-                            <input {...getInputProps()} />
-                            <Image src={coverImage} alt="Cover Image" layout="fill" />
-                            {coverImage === '/default-cover.png' && (
+                            <input {...getInputProps()} accept="image/*"/>
+                            <Image src={coverImage} alt="Cover Image" layout="fill" className={styles.imgProfile} />
                                 <Upload color="#ffffff" strokeWidth={2.25} className={styles.uploadIcon} />
-                            )}
                         </div>
                     )}
+                </Dropzone>
+            </div>
+            <div className={styles.profile}>
+                <Dropzone onDrop={(files: File[], _: FileRejection[], __: DropEvent) => handleDrop(files, 'profile')}>
+                {({ getRootProps, getInputProps }) => (
+                    <div {...getRootProps()} className={styles.imageContainerProfile}>
+                    <input {...getInputProps()} accept="image/*" />
+                    <Image src={profileImage} alt="Profile Image" layout="fill" className={styles.imgProfile}/>
+                    <Upload color="#ffffff" strokeWidth={2.25} className={styles.uploadIconProfile} />
+                    </div>
+                )}
                 </Dropzone>
             </div>
         </main>
