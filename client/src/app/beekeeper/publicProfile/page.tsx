4bed507932a8df7590c977343acdc372ca57d743
nicolas antonini <nicki.pablo123@gmail.com>
2024-10-23 14:20:38 -0300

    .

---
+++ client/src/app/beekeeper/publicProfile/page.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { ArrowLeftCircle } from "lucide-react";
+
+interface profileProps{
+    coverImage: string;
+    profileImage: string;
+    userName: string;
+    
+}
+
+export default function publicProfile({coverImage, profileImage, userName}: profileProps){
+    return(
+        <div className={styles.main}>
+
+            {/* FOTO DE CAPA */}
+            <div className={styles.cover}>
+                <div className={styles.imageContainer}>
+                    <Image
+                    src={coverImage}
+                    alt="Foto de capa do usuário"
+                    layout="full"
+                    className={styles.img}
+                    />
+                </div>
+            </div>
+
+            {/* FOTO DE PERFIL + O NOME DE PERFIL */}
+            <div className={styles.profile}>
+                <div className={styles.imageContainerProfile}>
+                    <Image
+                    src={profileImage}
+                    alt="Foto de capa do usuário"
+                    layout="full"
+                    className={styles.img}
+                    />
+                </div>
+                <p>{userName}</p>
+            </div>
+
+
+
+        </div>
+    )
+}
