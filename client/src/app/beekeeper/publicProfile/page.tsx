0fe372480e3e295902e6a7027b9befc398a6309a
nicolas antonini <nicki.pablo123@gmail.com>
2024-11-05 07:50:19 -0300

    .

--- client/src/app/beekeeper/publicProfile/page.tsx
+++ client/src/app/beekeeper/publicProfile/page.tsx
@@ -10,18 +10,18 @@ interface profileProps{
     profileImage: string;
     userName: string;
     userDesc: string;
-    nameProperty: string;
-    numHec: number;
+    nameApiary: string;
+    numHives: number;
 }
 
-export default function publicProfile({coverImage, profileImage, userName, userDesc, nameProperty, numHec}: profileProps){
+export default function profile({coverImage, profileImage, userName, userDesc, nameApiary, numHives}: profileProps){
 
-    userName = 'Nome do agricultor'
-    profileImage = '/farmer.svg'
+    userName = 'Laís Teixeira de Freitas'
+    profileImage = '/beekeeper.svg'
     coverImage = '/default-cover.png'
     userDesc = 'Descrição......'
-    nameProperty = 'Nome da propriedade'
-    numHec = 21
+    nameApiary = 'Apiário Freitas'
+    numHives = 21
 
     useEffect(() => {
         const initMap = () => {
@@ -106,7 +106,7 @@ export default function publicProfile({coverImage, profileImage, userName, userD
                     <input className={styles.input} type="checkbox"/>
                     <svg className={styles.svg} height="24px" id="Layer_1" version="1.2" viewBox="0 0 24 24" width="24px" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><g><g><path d="M9.362,9.158c0,0-3.16,0.35-5.268,0.584c-0.19,0.023-0.358,0.15-0.421,0.343s0,0.394,0.14,0.521    c1.566,1.429,3.919,3.569,3.919,3.569c-0.002,0-0.646,3.113-1.074,5.19c-0.036,0.188,0.032,0.387,0.196,0.506    c0.163,0.119,0.373,0.121,0.538,0.028c1.844-1.048,4.606-2.624,4.606-2.624s2.763,1.576,4.604,2.625    c0.168,0.092,0.378,0.09,0.541-0.029c0.164-0.119,0.232-0.318,0.195-0.505c-0.428-2.078-1.071-5.191-1.071-5.191    s2.353-2.14,3.919-3.566c0.14-0.131,0.202-0.332,0.14-0.524s-0.23-0.319-0.42-0.341c-2.108-0.236-5.269-0.586-5.269-0.586    s-1.31-2.898-2.183-4.83c-0.082-0.173-0.254-0.294-0.456-0.294s-0.375,0.122-0.453,0.294C10.671,6.26,9.362,9.158,9.362,9.158z"></path></g></g></svg>
                 </label>
-                <button className={styles.chatBtn}><Link href={'/Homepage/Chat'} className={styles.chatLink}>Enviar mensagem</Link></button>
+                <button className={styles.chatBtn}><Link href={'Homepage/Chat'} className={styles.chatLink}>Enviar mensagem</Link></button>
             </div>
 
             <div className={styles.more}>
@@ -149,17 +149,15 @@ export default function publicProfile({coverImage, profileImage, userName, userD
                     </div>
                 </section>
                 <div className={styles.farm}>
-                    <h1 className={styles.titleFarm}>Propriedade</h1>
-                    <p className={styles.nameFarm}>Nome da propriedade: {nameProperty}
+                    <h1 className={styles.titleFarm}>Apiário</h1>
+                    <p className={styles.nameFarm}>Nome do apiário: {nameApiary}
                     </p>
-                    <p className={styles.hecFarm}>Hectares de plantação: {JSON.stringify(numHec)}
+                    <p className={styles.hecFarm}>Colméias disponíveis: {JSON.stringify(numHives)}
                     </p>
 
                     <div className={styles.map}>
-
                     {/* Div onde o mapa será renderizado */}
-                    <div id="map" className={styles.map}>
-                        
+                    <div id="map" className={styles.map}> 
                     </div>
                     </div>
                 </div>
