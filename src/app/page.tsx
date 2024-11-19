90b0695c70fe54ada3a43c02c07dda20ea75f225
Nic-Antonini <nicki.pablo123@gmail.com>
2024-11-19 15:31:30 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {useState, useEffect,useLayoutEffect} from 'react';
+import React, {useState, useEffect, useLayoutEffect} from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Image from "next/image";
